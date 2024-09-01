0ba6da17fee7bb240a7747850ba6c3a2b74ee10a
DanielDiazDev <danieldiazdev00@gmail.com>
2024-09-01 13:00:43 -0300

    Mejore el trigger para cambio de escena y camara

--- Assets/Scripts/ZonaTrigger.cs
+++ Assets/Scripts/ZonaTrigger.cs
@@ -6,17 +6,21 @@ public class ZonaTrigger : MonoBehaviour
 {
     [SerializeField] private string _destinoZonaId;
     [SerializeField] private ZonaTrigger _triggerOpuesto;
-
+    [SerializeField] private Transform _teleport;
+    [SerializeField] private Transform _piojoTransform;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.CompareTag("Piojo"))
         {
             GameManager.Instance().CambiarZona(_destinoZonaId);
+            _piojoTransform.position = _teleport.position;
         }
         if (_triggerOpuesto != null)
         {
-            StartCoroutine(DesactivarTriggerTemporalmente());
+            //StartCoroutine(DesactivarTriggerTemporalmente());
         }
+
     }
 
     private IEnumerator DesactivarTriggerTemporalmente()
